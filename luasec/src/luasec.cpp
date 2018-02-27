45735b9bb7f58ce9d171baaecc5cc07543d98933
Björn Ritzl <bjorn.ritzl@gmail.com>
2018-02-27 08:23:23 +0100

    Fix for HTML5
    From https://github.com/subsoap/defold-luasec

--- luasec/src/luasec.cpp
+++ luasec/src/luasec.cpp
@@ -1,12 +1,15 @@
-// include the Defold SDK
+
+
 #if defined(WIN32)
 #include <Winsock2.h>
 #include <windows.h>
 #endif
 
+// include the Defold SDK
 #include <dmsdk/sdk.h>
-
 #include "config.h"
+
+#if defined(DM_PLATFORM_HTML5) != true
 #include "ssl.h"
 #include "context.h"
 #include "x509.h"
@@ -14,19 +17,19 @@
 static void LuaInit(lua_State* L)
 {
     int top = lua_gettop(L);
-    
+
     luaopen_ssl_core(L);
     //printf("%d\n", lua_gettop(L));
     lua_pop(L, 4);
-    
+
     luaopen_ssl_context(L);
     //printf("%d\n", lua_gettop(L));
     lua_pop(L, 5);
-    
+
     luaopen_ssl_x509(L);
     //printf("%d\n", lua_gettop(L));
     lua_pop(L, 3);
-    
+
     assert(top == lua_gettop(L));
 }
 
@@ -39,7 +42,7 @@ dmExtension::Result InitializeMyExtension(dmExtension::Params* params)
 {
     // Init Lua
     LuaInit(params->m_L);
-    
+
     printf("Registered %s Extension\n", MODULE_NAME);
     return dmExtension::RESULT_OK;
 }
@@ -60,3 +63,30 @@ dmExtension::Result FinalizeMyExtension(dmExtension::Params* params)
 // DM_DECLARE_EXTENSION(symbol, name, app_init, app_final, init, update, on_event, final)
 
 DM_DECLARE_EXTENSION(LuaSec, LIB_NAME, AppInitializeMyExtension, AppFinalizeMyExtension, InitializeMyExtension, 0, 0, FinalizeMyExtension)
+
+#else
+
+
+
+dmExtension::Result AppInitializeMyExtension(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result InitializeMyExtension(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result AppFinalizeMyExtension(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result FinalizeMyExtension(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+DM_DECLARE_EXTENSION(LuaSec, LIB_NAME, AppInitializeMyExtension, AppFinalizeMyExtension, InitializeMyExtension, 0, 0, FinalizeMyExtension)
+#endif
